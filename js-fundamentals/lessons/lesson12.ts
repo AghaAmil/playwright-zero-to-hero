e7a15047ab6efe466bbbf06ada06dbe9081b07eb
AghaAmil <moravveji.amir.92@gmail.com>
2024-12-21 18:50:47 +0330

    basic overview of typescript

---
+++ js-fundamentals/lessons/lesson12.ts
@@ -0,0 +1,18 @@
+let customerFirstName: string = 'Amirhossein';
+let customerLastName: string = 'Moravveji';
+let customerAge: number = 31;
+
+type customer = {
+    firstName: string,
+    lastName: string,
+    age: number,
+    active: boolean
+};
+
+let newCustomer: customer = {
+    firstName: 'Michael',
+    lastName: 'Jackson',
+    age: 60,
+    active: false
+};
+
